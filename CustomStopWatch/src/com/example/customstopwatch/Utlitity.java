b3f7275266a399bae326556dc801f169520c97ee
albertowusuasare <owusuasa@grinnell.edu>
2014-09-19 18:46:02 -0500

    Add logIn functionality
    * Facebook logIn button added
    * Text fields with application name added

---
+++ CustomStopWatch/src/com/example/customstopwatch/Utlitity.java
@@ -0,0 +1,15 @@
+package com.example.customstopwatch;
+
+/**
+ * This class is to provide Utilities that will be used throughout the App making making proc
+ * @author albertowusu-asare
+ */
+public class Utlitity {
+
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
